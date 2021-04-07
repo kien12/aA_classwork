fdd010edfc9eea829f6b3c3663fad50894270bd2
Kien Nguyen <nguyen_kien@live.com>
2021-04-06 22:21:51 -0500

    Added Employee class

--- W4D2/Employee.rb
+++ W4D2/Employee.rb
@@ -1,14 +1,25 @@
 class Employee
+    attr_reader :salary, :name, :title
+    attr_accessor :boss
 
     def initialize(name, title, salary, boss = nil)
         @name = name
         @title = title
         @salary = salary
         @boss = boss
+        # p darren = Manager.new('Darren', 'TA Manager', 78000, 'Ned')
+        # p darren.hire(self) unless boss == nil
+        # p super(name, title, salary, boss)
     end
 
     def bonus(multiplier)
         bonus = @salary * multiplier
+    end
+
+    def boss=(boss)
+        @boss = boss
+        boss.hire(self) unless boss.nil?
+        boss
     end
 
 end
@@ -22,19 +33,25 @@ class Manager < Employee
     end
 
     def hire(employee)
-        
+        employees << employee   
+        employee
     end
-#employee.boss.instance_of? Manager 
-# manager.employee << employee
+    
     def bonus(multiplier)
-        total_salary = 0
-        employees.each  do |employee|
-            total_salary += employee.salary
-        end
-        bonus = total_salary * multiplier
+        bonus = subsalary * multiplier
     end
-
-
+    
+    def subsalary
+        total_salary = 0
+        self.employees.each do |employee|
+            if employee.is_a?(Manager)
+                total_salary += employee.salary + employee.subsalary
+            else
+                total_salary += employee.salary
+            end
+        end
+        total_salary
+    end
 end
 
 
@@ -42,5 +59,10 @@ ned = Manager.new('Ned', 'Founder', 1000000)
 darren = Manager.new('Darren', 'TA Manager', 78000, 'Ned')
 shawna = Employee.new('Shawna', 'TA', 12000, 'Darren')
 david = Employee.new('David', 'TA', 10000, 'Darren')
-
-p ned.bonus(5)
+darren.hire(david)
+darren.hire(shawna)
+ned.hire(darren)
+# p ned.employees
+p ned.bonus(5) # => 500_000
+p darren.bonus(4) # => 88_000
+p david.bonus(3) # => 30_000
