e046e59963a2363a8f1cae412f30d792b1161ffe
Kien Nguyen <nguyen_kien@live.com>
2021-04-12 18:56:32 -0500

    Finished int sets

--- W5D1/skeleton/lib/p01_int_set.rb
+++ W5D1/skeleton/lib/p01_int_set.rb
@@ -83,19 +83,32 @@ class ResizingIntSet
     @count = 0
   end
 
+
   def insert(num)
+    if include?(num)
+      return false
+    end
+    self[num] << num
+    @count += 1
+    resize! if count == num_buckets
   end
 
   def remove(num)
+    if include?(num)
+      self[num].delete(num)
+      @count -= 1
+    end
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -103,5 +116,13 @@ class ResizingIntSet
   end
 
   def resize!
+    @count = 0
+    old_store = @store.dup
+    double = num_buckets * 2
+    @store = Array.new(double) {Array.new}
+    
+    old_store.flatten.each { |el| insert(el)}
+    @store
   end
+
 end
