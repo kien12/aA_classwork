dd0d4aa69ce304501a90145b341221e68622f6c3
Kien Nguyen <nguyen_kien@live.com>
2021-04-16 18:38:08 -0500

    bad years incomplete

--- W5D5/movie_buff/movie_buff/02_queries.rb
+++ W5D5/movie_buff/movie_buff/02_queries.rb
@@ -3,12 +3,16 @@ def eighties_b_movies
   # 3 and 5 (inclusive).
   # Show the id, title, year, and score.
 
-  Movie.select(:title)
+  Movie.select(:id, :title, :yr, :score)
+        .where(yr:1980..1989)
+        .where(score:3..5)
 end
 
 def bad_years
   # List the years in which a movie with a rating above 8 was not released.
-
+  Movie.select(:yr).where.not("score > ?", 8)
+  # .group(:yr)
+        
 end
 
 def cast_list(title)
