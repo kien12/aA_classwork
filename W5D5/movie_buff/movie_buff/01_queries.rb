f9fd7092b31fc298c3f4b3a1dde8eb44b6057a35
Josiah Leon <sjosiahleon@gmail.com>
2021-04-16 15:10:22 -0700

    Got Ford

--- W5D5/movie_buff/movie_buff/01_queries.rb
+++ W5D5/movie_buff/movie_buff/01_queries.rb
@@ -21,7 +21,10 @@ def harrison_ford
   #
   # Find the id and title of all movies in which Harrison Ford
   # appeared but not as a lead actor
-  Actor.joins(:movies).select(:id, :title).where(actors: {name: 'Harrison Ford'})
+  Movie.joins(:actors)
+       .select(:id, :title)
+       .where(actors: {name: 'Harrison Ford'})
+       .where.not(castings: { ord: 1 })
 
 end
 
@@ -39,6 +42,7 @@ def biggest_cast
   #
   # Find the id and title of the 3 movies with the
   # largest casts (i.e most actors)
+  Movie.joins(:actors).group(:movies).select(:id, :title, "COUNT(*) AS num_actors").distinct
 
 end
 
